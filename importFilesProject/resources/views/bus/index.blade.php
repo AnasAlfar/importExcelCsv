def6a477a62fe670db2f3c5b82f46cb0d71515bd
Anas Alfar <anasalfar548@gmail.com>
2024-02-22 23:03:07 +0300

    update on index page

--- importFilesProject/resources/views/bus/index.blade.php
+++ importFilesProject/resources/views/bus/index.blade.php
@@ -10,50 +10,78 @@
 <body>
 @include('partials.navbar')
 <br>
-<br>
-<form action="{{ route('bus.store') }}" method="post" enctype="multipart/form-data">
-    @csrf 
-    <input type="file" class="form-control" name="file">
-    <button type="submit" class="btn btn-primary">Import</button>
-
-</form>
-<br>
-<a href="{{ route('bus.create') }}" class="btn btn-success">Export</a>
-<br>
 
 @if (session('success'))
     <div class="alert alert-success">
         {{ session('success') }}
     </div>
 @endif
-<table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">bus #</th>
-      <th scope="col">shofair name</th>
-      <th scope="col">number</th>
-      <th scope="col">capacity</th>
-      
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($allBuses as $bus)
-        <tr>
-        <th scope="row">{{ $bus->bus_number }}</th>
-        <td>{{ $bus->shofir_name }}</td>
-        <td>{{ $bus->number }}</td>
-        <td>{{ $bus->capacity }}</td>
-        </tr>
-    @endforeach
-    
-  </tbody>
-</table>
-    
+<br>
+<div class="container">
+    <div class="row justify-content-end mb-3">
+        <div class="col-auto">
+            <form action="{{ route('bus.store') }}" method="post" enctype="multipart/form-data" id="uploadForm">
+                @csrf
+                
+                <input type="file" name="file" id="uploadFile" style="display: none;">
+                
+                <button class="btn btn-primary" type="button" onclick="document.getElementById('uploadFile').click();">
+                    Import
+                </button>
+            </form>
+        </div>
+        <div class="col-auto">
+            <a href="{{ route('bus.create') }}" class="btn btn-success">Export</a>
+        </div>
+    </div>
+</div>
 
 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">bus #</th>
+                        <th scope="col">shofair name</th>
+                        <th scope="col">number</th>
+                        <th scope="col">capacity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($allBuses as $bus)
+                    <tr>
+                        <th scope="row">{{ $bus->bus_number }}</th>
+                        <td>{{ $bus->shofir_name }}</td>
+                        <td>{{ $bus->number }}</td>
+                        <td>{{ $bus->capacity }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.custom-file-input').on('change', function(event) {
+            var inputFile = event.currentTarget;
+            $(inputFile).parent()
+                .find('.custom-file-label')
+                .html(inputFile.files[0].name);
+        });
+    });
+
+    document.getElementById('uploadFile').onchange = function(event) {
+        document.getElementById('uploadForm').submit();
+    };
+</script>
+
 
 </body>
 </html>
